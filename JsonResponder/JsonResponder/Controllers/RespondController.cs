9ce799088afaf546f277141e5ea578baa61eb943
Martyn Price <martyn.price@softwaresolved.com>
2022-03-18 14:45:59 +0000

    Add JSON Responder project
    Project uses Newtonsoft.Json nuget package to take the inbound string and attempt to serialise it into a json object and return that object both to the console and the sender.

---
+++ JsonResponder/JsonResponder/Controllers/RespondController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace JsonResponder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RespondController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            string rawContent;
+            using (var contentStream = this.Request.Body)
+            {
+                using (var sr = new StreamReader(contentStream))
+                {
+                    rawContent = await sr.ReadToEndAsync();
+                    Console.WriteLine(rawContent);
+                }
+            }
+            var json = Newtonsoft.Json.JsonConvert.DeserializeObject(rawContent);
+            if (json == null)
+                return BadRequest();
+            return Ok(json);
+        }
+    }
+}
